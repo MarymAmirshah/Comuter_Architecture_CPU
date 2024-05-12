4a9e8d62f6380c8d9692d46004a67010988e1e8b
Maryam Amirshahkarami <121711305+MarymAmirshah@users.noreply.github.com>
2024-05-12 20:39:34 +0330

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/puyapardaz/Desktop/Phase 1/ISE/Project_Phase2/testbench_isim_beh.exe" -prj "C:/Users/puyapardaz/Desktop/Phase 1/ISE/Project_Phase2/testbench_beh.prj" "work.testbench" "work.glbl" 
---
+++ testbench.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:32:53 06/09/2023
+// Design Name:   CPU
+// Module Name:   C:/Users/puyapardaz/Desktop/Phase 1/ISE/Project_Phase2/testbench.v
+// Project Name:  Project_Phase2
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: CPU
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module testbench;
+	reg clk;
+
+	initial begin
+		clk = 0;
+		repeat (200) 
+			#20 clk = ~clk;
+		$finish;
+	end
+
+	CPU cpu(.clk(clk));
+
+	initial begin
+
+		$dumpfile("wave.vcd");
+		$dumpvars(0, cpu);
+	end
+
+endmodule
+
